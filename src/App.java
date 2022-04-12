28bb7db57b04bd19ae522b07eedc45ee5adec296
lai2-01 <lai2-01@itismeucci.com>
2022-04-12 12:14:24 +0200

    main modified

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
     
+        Runnable c1 = new Bell("DIN",5);
+        Runnable c2 = new Bell ("DON", 5);
+        
+        Thread T1 = new Thread(c1);
+        Thread T2 = new Thread(c2);
+
+        T1.start();
+
+
+        T2.start();
+
+        
+
+        new Thread(new Bell("DAN", 5)).start();
+            
+        
     }
 }
